f65b81840652b9620c283013ac038cb0bdd4b271
Barbara Kos <101199634+barbarakos@users.noreply.github.com>
2022-12-21 15:08:20 +0100

    Update AppDependencies.swift

--- QuizApp/AppDependencies.swift
+++ QuizApp/AppDependencies.swift
@@ -37,10 +37,6 @@ extension Container {
         LoginViewModel(router: appRouter(), useCase: loginUseCase()) as LoginViewModel
     }
 
-    static let loginViewController = Factory {
-        LoginViewController(viewModel: loginViewModel()) as LoginViewController
-    }
-
     static let loginView = Factory {
         UIHostingController(rootView: LoginView(viewModel: loginViewModel())) as UIHostingController
     }
